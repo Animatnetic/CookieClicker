2df3cef06af39d2efef5ed8b9f23c3b036c45f96
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-06-23 22:17:35 +0400

    Initial Commit

---
+++ CookieClicker/CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieClickerApp.swift
+//  CookieClicker
+//
+//  Created by Faris Ansari on 23/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
