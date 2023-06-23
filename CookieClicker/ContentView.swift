06ba92579bd573a5e8ba1496efe79e1fd64ea180
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-06-23 22:26:42 +0400

    Update barebones
    Added barebones feature for the cookie clicker. No variables, no nothing, just what is set up as to build on top of for the cookie clicker app.

--- CookieClicker/ContentView.swift
+++ CookieClicker/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("0 cookies eaten")
+            
+            Button("OM NOM!"){
+                print("You have eaten a cookie")
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
