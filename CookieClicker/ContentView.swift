bcffe125e4026bfc66e612511b7e9a922be1d088
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-06-24 12:09:17 +0400

    More advanced feature of cookie clicker, added buy and reset button

--- CookieClicker/ContentView.swift
+++ CookieClicker/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var counter = 0
+    @State private var message = ""
+    @State private var cookiesInventory = 40 // We have
+    
     var body: some View {
         VStack {
-            Text("0 cookies eaten")
+            Spacer()
+            
+            Text("\(counter) cookies eaten")
+            Text("\(cookiesInventory) cookies remaining")
             
             Button {
-                print("You have eaten a cookie")
+                counter += 1
+                cookiesInventory -= 1
+                
+                if counter >= 40 {
+                    message = "Oh nooooooooooo 😱"
+                } else if counter >= 30 && counter <= 39 {
+                    message = "What are you doing??? 😰"
+                } else if counter >= 20 && counter <= 29 {
+                    message = "Don't eat too many cookies ☹️"
+                }
             } label: {
                 Text("Eat me")
                     .padding()
@@ -22,9 +39,36 @@ struct ContentView: View {
                     .cornerRadius(10)
             }
             .shadow(color: .black, radius:10)
+            
+            Text(message)
+            
+            Spacer()
+            
+            HStack {
+                Button {
+                    cookiesInventory = 40
+                    counter = 0
+                } label: {
+                    Text("Reset cookies")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .cornerRadius(10)
+                }
+                
+                Button {
+                    cookiesInventory += 1
+                } label: {
+                    Text("Buy cookie")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.brown)
+                        .cornerRadius(10)
+                }
+            }
         }
     }
-        
+
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
